d1b713e34e2092ed9ba1676590de90cdafcb0f93
bbax <john_oliphant@hotmail.com>
2015-04-22 16:54:12 -0400

    Updating files
    Uploading necessary files to Github

---
+++ Needed packages.R
@@ -0,0 +1,38 @@
+install.packages(c(
+    "foreign",
+    "RWinEdt",
+    "MASS",
+    "ggplot2",
+    "knitr",
+#    "cacheSweave",
+    "markdown",
+    "tools",
+    "stringr",
+    "qdap",
+    "Hmisc",
+    "stargazer",
+    "apsrtable",
+    "Hmisc",
+    "reshape2",
+    "survey",
+    "sandwich",
+    "lmtest"
+))
+
+
+library("foreign")
+library("RWinEdt")
+library("MASS")
+library("ggplot2")
+library("knitr")
+library("qdap")
+library("Hmisc")
+library("stargazer")
+library("apsrtable")
+library("Hmisc")
+library("reshape2")
+library("survey")
+#library("cacheSweave")
+library("markdown")
+library("tools")
+library("stringr")
